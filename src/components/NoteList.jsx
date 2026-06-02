f7fa864a92171e32be3b57845cdf8596f7d30a4d
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-02 11:09:55 +0700

    created NoteList component

---
+++ src/components/NoteList.jsx
@@ -0,0 +1,31 @@
+import NoteItem from './NoteItem';
+import PropTypes from 'prop-types';
+
+export default function NoteList({ notes }) {
+  return (
+    <div className='notes-list'>
+      {notes.map((note) => (
+        <NoteItem
+          key={note.id}
+          id={note.id}
+          title={note.title}
+          body={note.body}
+          createdAt={note.createdAt}
+          archived={note.archived}
+        />
+      ))}
+    </div>
+  );
+}
+
+NoteList.propTypes = {
+  notes: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+      archived: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { getActiveNotes } from '../utils/local-data';
 import NoteEmpty from '../components/NoteEmpty';
+import NoteList from '../components/NoteList';
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -12,7 +13,7 @@ class HomePage extends React.Component {
 
   render() {
     const { notes } = this.state;
-    return notes.length === 0 ? <NoteEmpty /> : <h2>HAI</h2>;
+    return notes.length === 0 ? <NoteEmpty /> : <NoteList notes={notes} />;
   }
 }
 
