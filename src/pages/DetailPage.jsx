465372e2c2f272e9fb7ba3b833f9343f65847044
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-02 11:27:26 +0700

    implemented data from parent to detail page

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,13 +1,29 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { getNote } from '../utils/local-data';
+import { showFormattedDate } from '../utils';
+
 function DetailPageWrapper() {
-  return <DetailPage />;
+  const { id } = useParams();
+  return <DetailPage id={id} />;
 }
 
 class DetailPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      note: getNote(props.id),
+    };
   }
   render() {
-    return <div>Detail Page</div>;
+    return (
+      <div className='detail-page'>
+        <h2 className='detail-page__title'>{this.state.note.title}</h2>
+        <p className='detail-page__createdAt'>{showFormattedDate(this.state.note.createdAt)}</p>
+        <div className='detail-page__body'>{this.state.note.body}</div>
+      </div>
+    );
   }
 }
 
