e437d3075d77d68a5cadf41bf9121dd7fd093f7a
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-02 10:39:46 +0700

    add react-router components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className='app-container'>
       <header>
-        <h1>Aplikasi Catatan</h1>
+        <h1>
+          <Link to='/'>Aplikasi Catatan</Link>
+        </h1>
         <div className='navigation'>
           <ul>
-            <li>Arsip</li>
+            <li>
+              <Link to='/archives'>Arsip</Link>
+            </li>
           </ul>
         </div>
       </header>
-      <main>HI</main>
+      <main>
+        <Routes>
+          <Route
+            path='/'
+            element={<h2>Halaman Beranda</h2>}
+          />
+        </Routes>
+      </main>
     </div>
   );
 }
