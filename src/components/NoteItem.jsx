19f6c521d2785b359131d3cb1502d4da4aad5103
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-02 11:20:40 +0700

    apply link react-router to each note title

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
 import { showFormattedDate } from '../utils';
+import { Link } from 'react-router-dom';
 
 export default function NoteItem({ id, title, body, createdAt, archived }) {
   return (
     <div className='note-item'>
-      <h3 className='note-item__title'>{title}</h3>
+      <h3 className='note-item__title'>
+        <Link to={`/notes/${id}`}>{title}</Link>
+      </h3>
       <p className='note-item__createdAt'>{showFormattedDate(createdAt)}</p>
       <p className='note-item__body'>{body}</p>
     </div>
