47049af3516455a50fbb09c6163e1a123474ca25
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-02 14:15:09 +0700

    swapped position between delete and archive buttons

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -53,27 +53,6 @@ function AddNoteActionButton({ onAdd }) {
 function DetailActionButton({ onDelete, onArchive, onUnarchive, archived }) {
   return (
     <>
-      <button
-        className='action'
-        onClick={onDelete}
-      >
-        <svg
-          stroke='currentColor'
-          fill='currentColor'
-          stroke-width='0'
-          viewBox='0 0 24 24'
-          height='1em'
-          width='1em'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path
-            fill='none'
-            d='M0 0h24v24H0V0z'
-          ></path>
-          <path d='M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z'></path>
-        </svg>
-      </button>
-
       {!archived ? (
         <button
           className='action'
@@ -117,6 +96,27 @@ function DetailActionButton({ onDelete, onArchive, onUnarchive, archived }) {
           </svg>
         </button>
       )}
+
+      <button
+        className='action'
+        onClick={onDelete}
+      >
+        <svg
+          stroke='currentColor'
+          fill='currentColor'
+          stroke-width='0'
+          viewBox='0 0 24 24'
+          height='1em'
+          width='1em'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            fill='none'
+            d='M0 0h24v24H0V0z'
+          ></path>
+          <path d='M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z'></path>
+        </svg>
+      </button>
     </>
   );
 }
