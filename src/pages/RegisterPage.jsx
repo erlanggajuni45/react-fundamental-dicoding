1e8eb73025a02e7ba9096e7f9f307c9c0e6f40c3
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-03 17:43:18 +0700

    applied useInput hook to register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,12 @@
 import { Link } from 'react-router-dom';
+import useInput from '../hooks/useInput';
 
 function RegisterPage() {
+  const [name, onNameChange] = useInput('');
+  const [email, onEmailChange] = useInput('');
+  const [password, onPasswordChange] = useInput('');
+  const [confirmPassword, onConfirmPasswordChange] = useInput('');
+
   return (
     <div>
       <h2>Isi form untuk mendaftar akun.</h2>
@@ -11,24 +17,32 @@ function RegisterPage() {
           type='text'
           id='name'
           name='name'
+          value={name}
+          onChange={onNameChange}
         />
         <label htmlFor='email'>Email</label>
         <input
           type='email'
           id='email'
           name='email'
+          value={email}
+          onChange={onEmailChange}
         />
         <label htmlFor='password'>Password</label>
         <input
           type='password'
           id='password'
           name='password'
+          value={password}
+          onChange={onPasswordChange}
         />
         <label htmlFor='confirmPassword'>Konfirmasi Password</label>
         <input
           type='password'
           id='confirmPassword'
           name='confirmPassword'
+          value={confirmPassword}
+          onChange={onConfirmPasswordChange}
         />
         <button type='submit'>Daftar</button>
       </form>
