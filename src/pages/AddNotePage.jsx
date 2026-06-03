164d7a1f389c21bbf9fa748780d53cc2a4233d49
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-03 21:38:25 +0700

    implemented locale on add note page

--- src/pages/AddNotePage.jsx
+++ src/pages/AddNotePage.jsx
@@ -3,15 +3,23 @@ import ActionButtonContainer from '../components/ActionButtons';
 import { addNote } from '../utils/network-data';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import GlobalContext from '../context/GlobalContext';
 
 function AddNotePageWrapper() {
   const navigate = useNavigate();
+  const { locale } = useContext(GlobalContext);
 
   function onAddNoteEventHandler(note) {
     addNote(note);
     navigate('/');
   }
-  return <AddNotePage onAddNote={onAddNoteEventHandler} />;
+  return (
+    <AddNotePage
+      onAddNote={onAddNoteEventHandler}
+      locale={locale}
+    />
+  );
 }
 
 class AddNotePage extends React.Component {
@@ -47,7 +55,11 @@ class AddNotePage extends React.Component {
   onAddNoteEventHandler() {
     const { title, body } = this.state;
     if (!title.trim() || !body.trim()) {
-      alert('Title dan body tidak boleh kosong!');
+      alert(
+        this.props.locale === 'id'
+          ? 'Title dan body tidak boleh kosong!'
+          : 'Title and body cannot be empty!',
+      );
       return;
     }
     this.props.onAddNote({ title, body });
@@ -59,13 +71,15 @@ class AddNotePage extends React.Component {
         <div className='add-new-page__input'>
           <div
             className='add-new-page__input__title'
-            data-placeholder='Catatan rahasia'
+            data-placeholder={this.props.locale === 'id' ? 'Catatan rahasia' : 'Secret note'}
             contentEditable
             onInput={this.onTitleChangeEventHandler}
           />
           <div
             className='add-new-page__input__body'
-            data-placeholder='Sebenarnya saya adalah ....'
+            data-placeholder={
+              this.props.locale === 'id' ? 'Sebenarnya saya adalah ....' : 'Actually I am ....'
+            }
             contentEditable
             onInput={this.onBodyChangeEventHandler}
           />
@@ -81,6 +95,7 @@ class AddNotePage extends React.Component {
 
 AddNotePage.propTypes = {
   onAddNote: PropTypes.func.isRequired,
+  locale: PropTypes.oneOf(['id', 'en']).isRequired,
 };
 
 export default AddNotePageWrapper;
