986cfc65960dec77c3001dfd8ba9d67d0c813631
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-03 21:38:07 +0700

    implemented locale on note empty component

--- src/components/NoteEmpty.jsx
+++ src/components/NoteEmpty.jsx
@@ -1,7 +1,13 @@
+import { useContext } from 'react';
+import GlobalContext from '../context/GlobalContext';
+
 function NoteEmpty() {
+  const { locale } = useContext(GlobalContext);
   return (
     <div className='notes-list-empty'>
-      <p className='notes-list-empty__message'>Tidak ada catatan</p>
+      <p className='notes-list-empty__message'>
+        {locale === 'id' ? 'Tidak ada catatan' : 'No notes'}
+      </p>
     </div>
   );
 }
