a66a8a9299c08a2d3755064f48ff0989b408b328
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-03 21:38:44 +0700

    implemented locale on not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,16 @@
+import { useContext } from 'react';
+import GlobalContext from '../context/GlobalContext';
+
 function NotFoundPage() {
+  const { locale } = useContext(GlobalContext);
   return (
     <div>
-      <h2>404 Not Found</h2>
-      <p>Halaman yang Anda cari tidak ditemukan.</p>
+      <h2>{locale === 'id' ? '404 Tidak Ditemukan' : '404 Not Found'}</h2>
+      <p>
+        {locale === 'id'
+          ? 'Halaman yang Anda cari tidak ditemukan.'
+          : 'The page you are looking for does not exist.'}
+      </p>
     </div>
   );
 }
