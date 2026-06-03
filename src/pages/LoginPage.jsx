5506759fdad1d9d313227f760c47d85f757863b5
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-03 18:49:12 +0700

    implemented useInput hook on login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,10 @@
+import useInput from '../hooks/useInput';
+import { Link } from 'react-router-dom';
+
 function LoginPage() {
+  const [email, setEmail] = useInput('');
+  const [password, setPassword] = useInput('');
+
   return (
     <>
       <h2>Yuk, login untuk menggunakan aplikasi.</h2>
@@ -8,15 +14,22 @@ function LoginPage() {
           type='email'
           id='email'
           name='email'
+          value={email}
+          onChange={setEmail}
         />
         <label htmlFor='password'>Password</label>
         <input
           type='password'
           id='password'
           name='password'
+          value={password}
+          onChange={setPassword}
         />
         <button type='submit'>Login</button>
       </form>
+      <p>
+        Belum punya akun? <Link to='/register'>Daftar di sini</Link>
+      </p>
     </>
   );
 }
